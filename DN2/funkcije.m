456364de1fd6e518ad4c351c04d92f8a9382e78d
maticplut <146855475+maticplut@users.noreply.github.com>
2023-11-12 15:46:31 +0100

    Add files via upload

---
+++ DN2/funkcije.m
@@ -0,0 +1,36 @@
+
+tocke[n_]:= RandomReal[{-1,1},{n,2}];
+
+
+
+preveri[t_]:=Module[{zun={},not={},n=Length[t],i},
+i=1;
+While[i <=n,
+If[t[[i]][[1]]^2+t[[i]][[2]]^2<=1,
+not=Append[not,t[[i]]],
+zun=Append[zun,t[[i]]]
+];
+i++;
+];
+{not,zun}
+];
+
+
+izracunPi[not_, zun_] := Module[{priblizekPi, napaka},
+   priblizekPi = N[(4 Length[not])/(Length[not] + Length[zun])];
+   napaka = Abs[priblizekPi - Pi];
+   {priblizekPi, napaka}
+   ];
+
+calcPi[] := Module[{i, skPrib = {}, skNpk = {}},
+   i = 500;
+   x = Range[500, 20000, 500];
+   While[i <= 20000, Module[{prib, npk},
+     {prib, npk} = izracunPi[preveri[tocke[i]]];
+     skPrib = Append[skPrib, prib];
+     skNpk = Append[skNpk, npk];
+     ];
+    i += 500;
+    ];
+   {skPrib, skNpk}
+   ];
